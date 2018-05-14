c491090db3e4c07386f91e5f9e40211abc032b14
aidansedgewick <aidansedgewick203@gmail.com>
2018-05-14 13:43:20 +0200

    add app context

--- bigHarvest.py
+++ bigHarvest.py
@@ -1,5 +1,6 @@
 import datetime 
 from inspire_crawler.tasks import schedule_crawl
+from flask import Flask
 
 today = datetime.datetime.today()
 
@@ -10,12 +11,15 @@ dates = [ str( today-datetime.timedelta(days=x) ).split()[0] for x in xrange(0,N
 
 list_of_sets = "physics,math,cs,econ,eess,q-bio,q-fin,stat"
 
-for start, stop in zip(dates[:-1], dates[1:]):
+app = Flask(__name__)
+with app.app_context():
+    for start, stop in zip(dates[:-1], dates[1:]):
 
-    from_date = "from_date='%s'" % start
-    until_date = "until_date='%s'" % stop
+        from_date = "from_date='%s'" % start
+        until_date = "until_date='%s'" % stop
 
-    
-    schedule_crawl("arXiv", "article", sets=list_of_sets,from_date="%s" %start, until_date="%s" %stop )
+        
+        
+        schedule_crawl("arXiv", "article", sets=list_of_sets,from_date="%s" %start, until_date="%s" %stop )
 
-    print from_date, until_date
+        print from_date, until_date
