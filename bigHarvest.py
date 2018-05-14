aaa6c3f8d684c31314797673c8f0afbdae7ed4f7
aidansedgewick <aidansedgewick203@gmail.com>
2018-05-14 13:32:18 +0200

    run schedule_crawl in bigHarvest

--- bigHarvest.py
+++ bigHarvest.py
@@ -8,10 +8,14 @@ Ndays = 100
 
 dates = [ str( today-datetime.timedelta(days=x) ).split()[0] for x in xrange(0,Ndays,delta_days) ]
 
+list_of_sets = "physics,math,cs,econ,eess,q-bio,q-fin,stat"
+
 for start, stop in zip(dates[:-1], dates[1:]):
 
     from_date = "from_date='%s'" % start
     until_date = "until_date='%s'" % stop
-    schedule_crawl("arXiv", "article", sets="physics,math,cs,econ,eess,q-bio,q-fin,stat",from_date="%s", until_date="%s" %(start,stop) )
+
+    
+    schedule_crawl("arXiv", "article", sets=list_of_sets,from_date="%s" %start, until_date="%s" %stop )
 
     print from_date, until_date
